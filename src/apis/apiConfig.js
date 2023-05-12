63746bf7a768724781a7f4c1c54fb3203fd66cb0
Miles Morris <milesmorris123@gmail.com>
2023-05-12 09:31:25 +0100

    changing prod url

--- src/apis/apiConfig.js
+++ src/apis/apiConfig.js
@@ -2,7 +2,7 @@ let apiUrl;
 const expressPort = 5002;
 const apiUrls = {
   development: `http://localhost:${expressPort}/api`,
-  production: `https://floating-chamber-62558.herokuapp.com/api`
+  production: `https://desolate-anchorage-87503.herokuapp.com/api`
 }
 
 if (window.location.hostname === 'localhost') {
